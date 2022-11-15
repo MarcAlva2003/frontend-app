5801f4c483afec4759929de6e3b8395b9bd31390
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-11-14 21:37:18 -0300

    Feat: add company form

--- src/app/user-panel/add-company/add-company-style.ts
+++ src/app/user-panel/add-company/add-company-style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
+import { pageFormat } from "../../../ui/styles/page-format";
 
 export const AddCompanyStyle = styled.div`
-
+  width: 100%;
+  .content{
+    ${pageFormat}
+  }
 `
--- src/app/user-panel/add-company/add-company.tsx
+++ src/app/user-panel/add-company/add-company.tsx
@@ -1,9 +1,135 @@
+import { useForm } from "react-hook-form";
 import { AddCompanyStyle } from "./add-company-style";
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+
 
 export const AddCompany = () => {
-  return(
+  const COUNTRIES_API_BASE_URL = 'https://restcountries.com';
+  const COUNTRIES_API_PATH = '/v3.1/all';
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  // Register: registra los campos del formulario
+  // handleSubmit: accion cuando se le hace submit al formulario
+  // formState: nos da informacion del campo, si fue tocado, visitado, errores, etc
+  //defaultValues para colocar calores por default (útil en editar compañia)
+  // -->  useForm( { defaultValues: {name: 'asdasd', bussiness:'adsadas'} } );
+  const [countriesList, setCountriesList] = useState<string[]>([]);
+  useEffect(() => {
+    axios.get(`${COUNTRIES_API_BASE_URL}${COUNTRIES_API_PATH}`)
+      .then(res => {
+        res.data.length > 0
+          ? setCountriesList(res.data.map((item: any) => { return item?.name?.common }).sort())
+          : setCountriesList(res.data)
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }
+
+  return (
     <AddCompanyStyle>
-      ADD COMPANY
+      <div className="add-company-bg">
+        <div className="content">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <label>Company name</label>
+              <input
+                type="text"
+                placeholder="Company name"
+                {
+                ...register('name', {
+                  required: true,
+                  maxLength: 200,
+                  pattern: /^[A-Za-zÀ-ú0-9-ñÑ ]+$/
+                })
+                }
+              />
+              <div>
+                {errors.name?.type === 'required' && (
+                  <p>The name of the company is requiered</p>
+                )}
+                {errors.name?.type === 'maxLength' && (
+                  <p>The name cannot be longer than 200 characters</p>
+                )}
+                {errors.name?.type === 'pattern' && (
+                  <p>The name can only have letters and numbers</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label>Bussiness</label>
+              <input
+                type="text"
+                placeholder="Company bussiness"
+                {
+                ...register('bussiness', {
+                  required: true,
+                  maxLength: 200,
+                  pattern: /^[A-Za-zÀ-ú0-9-ñÑ ]+$/
+                })
+                }
+              />
+              <div>
+                {errors.bussiness?.type === 'required' && (
+                  <p>The bussiness of the company is requiered</p>
+                )}
+                {errors.bussiness?.type === 'maxLength' && (
+                  <p>The bussiness cannot be longer than 200 characters</p>
+                )}
+                {errors.bussiness?.type === 'pattern' && (
+                  <p>The bussiness can only have letters and numbers</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label>Foundation date</label>
+              <input
+                type="date"
+                placeholder="Foundation date"
+                {
+                ...register('foundation_date', {
+                  required: true,
+                })
+                }
+              />
+              <div>
+                {errors.foundation_date?.type === 'required' && (
+                  <p>The bussiness of the company is requiered</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label>Foundation country</label>
+              <select
+                placeholder="Select country"
+                {...register('foundation_country', {
+                  required: true
+                })}
+              >
+                {countriesList.length > 0 ? (
+
+                  countriesList?.map((item) => (
+                    <option value={`${item}`}>{item}</option>
+                  ))
+                ) : (
+                  <option value="Loading...">loading...</option>
+                )}
+              </select>
+              <div>
+                {errors.foundation_country?.type === 'required' && (
+                  <p>The bussiness of the company is requiered</p>
+                )}
+              </div>
+            </div>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
     </AddCompanyStyle>
   )
 }
